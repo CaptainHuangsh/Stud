5e494ba64f2bb47896d834a8d5a7ccee10331e3c
huangshaohua <huangshaohuamail@gmail.com>
2017-03-05 20:20:51 +0800

    service实践

--- service/src/main/java/com/owen/service/MainActivity.java
+++ service/src/main/java/com/owen/service/MainActivity.java
@@ -1,6 +1,10 @@
 package com.owen.service;
 
+import android.content.ComponentName;
+import android.content.Intent;
+import android.content.ServiceConnection;
 import android.os.Handler;
+import android.os.IBinder;
 import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -17,6 +21,25 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     public static final int UPDATE_TEXT = 1;
     private Button btn;
     private Button btn2;
+    private Button btn3;
+    private Button btn4;
+    private Button btn5;
+    private Button btn6;
+    private MyService.DownloadBinder downloadBinder;
+    private ServiceConnection connection = new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName name, IBinder service) {
+            downloadBinder = (MyService.DownloadBinder) service;
+            downloadBinder.startDownload();
+            downloadBinder.getProgress();
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName name) {
+
+        }
+    };
+
     private TextView txt;
     private Handler handler = new Handler() {
         public void handleMessage(Message msg) {
@@ -40,9 +63,17 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         setContentView(R.layout.activity_main);
         btn = (Button) findViewById(R.id.button);
         btn2 = (Button) findViewById(R.id.button2);
+        btn3 = (Button) findViewById(R.id.start_service);
+        btn4 = (Button) findViewById(R.id.stop_service);
+        btn5 = (Button) findViewById(R.id.bind_service);
+        btn6 = (Button) findViewById(R.id.unbind_service);
         txt = (TextView) findViewById(R.id.text1);
         btn2.setOnClickListener(this);
         btn.setOnClickListener(this);
+        btn3.setOnClickListener(this);
+        btn4.setOnClickListener(this);
+        btn5.setOnClickListener(this);
+        btn6.setOnClickListener(this);
     }
 
     @Override
@@ -69,6 +100,19 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                         Log.i("button2", "is tochanged");
                     }
                 }).start();//不要忘了.start();
+                break;
+            case R.id.start_service:
+                startService(new Intent(this, MyService.class));
+                break;
+            case R.id.stop_service:
+                stopService(new Intent(this, MyService.class));
+                break;
+            case R.id.bind_service:
+                Intent bindIntent = new Intent(this,MyService.class);
+                bindService(bindIntent,connection,BIND_AUTO_CREATE);//绑定服务
+                break;
+            case R.id.unbind_service:
+                unbindService(connection);//接触绑定
                 break;
             default:
                 break;
---
+++ service/src/main/java/com/owen/service/MyService.java
@@ -0,0 +1,94 @@
+package com.owen.service;
+
+import android.app.Notification;
+import android.app.PendingIntent;
+import android.app.Service;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Binder;
+import android.os.IBinder;
+import android.support.annotation.IntDef;
+import android.support.annotation.Nullable;
+import android.util.Log;
+
+/**
+ * Created by owen on 2017/5/1.
+ */
+
+public class MyService extends Service {
+    private DownloadBinder mBinder = new DownloadBinder();
+    private Context mContext;
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return mBinder;
+    }
+
+
+    /**
+     * 服务创建时调用
+     */
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Log.d("MyService", "onCreate");
+        mContext = getApplication();
+        Notification notification;
+//        = new Notification(R.mipmap.ic_launcher, "notification comes",
+//                System.currentTimeMillis());
+        Intent notificationIntent = new Intent(this, MainActivity.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, notificationIntent, 0);
+        notification = new Notification.Builder(mContext)
+                .setContentTitle("huangshaohua")
+                .setContentText("haoshuai")
+                .setSmallIcon(R.mipmap.ic_launcher)
+                .build();
+
+//        notification.setLatestEventInfo(this,"this is tittle","this is content",pendingIntent);
+        //上面的方法已经被弃用了，现在时Builder方法
+        startForeground(1, notification);
+
+        /*Notification notification = new Notification.Builder(mContext)
+                .setContentTitle("huangshaohua")
+                .setContentText("haoshuai")
+                .setSmallIcon(R.mipmap.ic_launcher)
+                .build();*/
+
+    }
+
+    /**
+     * 每次启动时调用
+     *
+     * @param intent
+     * @param flags
+     * @param startId
+     * @return
+     */
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        Log.d("MyService", "onStartCommand");
+        return super.onStartCommand(intent, flags, startId);
+    }
+
+    /**
+     * 服务销毁时调用
+     */
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Log.d("MyService", "onDestroy");
+    }
+
+    class DownloadBinder extends Binder {
+
+        public void startDownload() {
+            Log.d("MyService", "startDownload");
+        }
+
+        public int getProgress() {
+            Log.d("MyService", "getProgress");
+            return 0;
+        }
+    }
+}
