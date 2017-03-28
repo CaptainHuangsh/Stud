0758c714542789229778484b178c6b3cea15a6cc
huangshaohua <huangshaohuamail@gmail.com>
2017-03-28 09:23:52 +0800

    commit

--- fragment/src/main/java/com/owen/fragment/MainActivity.java
+++ fragment/src/main/java/com/owen/fragment/MainActivity.java
@@ -1,7 +1,11 @@
 package com.owen.fragment;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button btn = (Button)findViewById(R.id.left_btn);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AnotherRightFragment fragment = new AnotherRightFragment();
+                FragmentManager fragmentManager = getFragmentManager();
+                FragmentTransaction transaction = fragmentManager.beginTransaction();
+                transaction.replace(R.id.right_fragment,fragment);
+                transaction.commit();
+            }
+        });
     }
 }
