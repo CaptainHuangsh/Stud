53a5494e28ff2cd2870161dc7cbbf72c92cdccff
huangshaohua <huangshaohuamail@gmail.com>
2017-04-02 23:23:48 +0800

    commit

--- activitylife/src/main/java/com/owen/activitylife/DialogActivity.java
+++ activitylife/src/main/java/com/owen/activitylife/DialogActivity.java
@@ -1,6 +1,9 @@
 package com.owen.activitylife;
 
 import android.app.Activity;
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.annotation.Nullable;
 
 /**
  * Created by owen on 2017/4/29.
@@ -8,4 +11,8 @@ import android.app.Activity;
 
 public class DialogActivity extends Activity{
 
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+    }
 }
