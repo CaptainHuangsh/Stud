c892db4216db4b94fca7d5ce944dfdd6edf1c5c0
huangshaohua <huangshaohuamail@gmail.com>
2017-04-02 23:23:37 +0800

    commit

--- activitylife/src/main/java/com/owen/activitylife/DialogActivity.java
+++ activitylife/src/main/java/com/owen/activitylife/DialogActivity.java
@@ -1,8 +1,11 @@
 package com.owen.activitylife;
 
+import android.app.Activity;
+
 /**
  * Created by owen on 2017/4/29.
  */
 
-public class DialogActivity {
+public class DialogActivity extends Activity{
+
 }
