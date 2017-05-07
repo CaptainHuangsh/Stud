9f9d308508d2762ddd2f2a5ee77cadb35d2c8d55
huangshaohua <huangshaohuamail@gmail.com>
2017-05-07 23:04:51 +0800

    利用hashmap绑定listView item和各个main类

--- app/src/main/java/com/example/owen/stud/MainActivity.java
+++ app/src/main/java/com/example/owen/stud/MainActivity.java
@@ -1,7 +1,5 @@
 package com.example.owen.stud;
 
-import android.app.Notification;
-import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -9,40 +7,52 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
-import android.widget.Toast;
 
 import com.example.owen.stud.activitylife.ActivityMain;
 import com.example.owen.stud.broadcast.BroadcastMain;
-import com.example.owen.stud.fragment.Fragment2;
 import com.example.owen.stud.fragment.FragmentMain;
 import com.example.owen.stud.http.HttpMain;
 import com.example.owen.stud.notification.NotificationMain;
 import com.example.owen.stud.service.ServiceMain;
 import com.example.owen.stud.settings.SettingMain;
 
+import java.util.HashMap;
+
 public class MainActivity extends AppCompatActivity {
     private String[] items = {
             "NetWork", "ActivityLife", "Fragment", "Broadcast", "Notification"
             , "Service", "Settings"
     };
+    HashMap<String, Class<?>> hs = new HashMap<>();
+
+    public void init() {
+        hs.put("NetWork", HttpMain.class);
+        hs.put("ActivityLife", ActivityMain.class);
+        hs.put("Fragment", FragmentMain.class);
+        hs.put("Broadcast", BroadcastMain.class);
+        hs.put("Notification", NotificationMain.class);
+        hs.put("Service", ServiceMain.class);
+        hs.put("Settings", SettingMain.class);
+    }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        init();
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(MainActivity.this
-        ,android.R.layout.simple_list_item_1,items);
-        ListView listView = (ListView)findViewById(R.id.list_item);
+                , android.R.layout.simple_list_item_1, items);
+        ListView listView = (ListView) findViewById(R.id.list_item);
         listView.setAdapter(adapter);
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                launch(SettingMain.class);
+                launch(hs.get(items[position]));
             }
         });
     }
 
-    public void launch(Class<?> cls){
-        startActivity(new Intent(MainActivity.this,cls));
+    public void launch(Class<?> cls) {
+        startActivity(new Intent(MainActivity.this, cls));
     }
 }
