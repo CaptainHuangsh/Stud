8eb3821c276ce243de61f202290e3c4f8cf4430b
owen <huangshaohuamail@gmail.com>
2017-07-26 11:38:49 +0800

    绘制简单view动画

---
+++ app/src/main/java/com/example/owen/stud/viewpaint/ViewPaintDirect.java
@@ -0,0 +1,8 @@
+package com.example.owen.stud.viewpaint;
+
+/**
+ * Created by loopeer on 2017/7/26.
+ */
+
+public class ViewPaintDirect {
+}
---
+++ app/src/main/java/com/example/owen/stud/viewpaint/ViewPaintMain.java
@@ -0,0 +1,8 @@
+package com.example.owen.stud.viewpaint;
+
+/**
+ * Created by loopeer on 2017/7/25.
+ */
+
+public class ViewPaintMain {
+}
